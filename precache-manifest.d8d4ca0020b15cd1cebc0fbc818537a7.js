6e98614648dc759c7a08215b7ee41c7da62f5ec8
morteza <morset65@gamil.com>
2020-09-08 02:14:17 +0430

    Updates

---
+++ precache-manifest.d8d4ca0020b15cd1cebc0fbc818537a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a119d210e98a773575ce6572d9ad8c9a",
+    "url": "/reactwebsite2/index.html"
+  },
+  {
+    "revision": "334fc79f967b45f0045c",
+    "url": "/reactwebsite2/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "be5124bb482ec3ca3f55",
+    "url": "/reactwebsite2/static/css/main.4d9ec858.chunk.css"
+  },
+  {
+    "revision": "334fc79f967b45f0045c",
+    "url": "/reactwebsite2/static/js/2.d9b2e2c5.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite2/static/js/2.d9b2e2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5124bb482ec3ca3f55",
+    "url": "/reactwebsite2/static/js/main.7eaa076a.chunk.js"
+  },
+  {
+    "revision": "b036e6830a63eb53a5ec",
+    "url": "/reactwebsite2/static/js/runtime-main.b43e001b.js"
+  },
+  {
+    "revision": "32703a8ed2cc23ff47f89cd33f10911f",
+    "url": "/reactwebsite2/static/media/img1.32703a8e.jpg"
+  },
+  {
+    "revision": "6fcd6ce38456d4660dc0ff0fed504865",
+    "url": "/reactwebsite2/static/media/img2.6fcd6ce3.jpg"
+  },
+  {
+    "revision": "378b90dd2075666f43b20262b5d35d88",
+    "url": "/reactwebsite2/static/media/img3.378b90dd.jpg"
+  },
+  {
+    "revision": "3da1a44f68de73fb32bebcbcac138452",
+    "url": "/reactwebsite2/static/media/img4.3da1a44f.jpg"
+  },
+  {
+    "revision": "39e9a85f078c17781a477d438e4116b2",
+    "url": "/reactwebsite2/static/media/img5.39e9a85f.jpg"
+  },
+  {
+    "revision": "8d57bb27fe2469c315a7f535b28e46d0",
+    "url": "/reactwebsite2/static/media/img6.8d57bb27.jpg"
+  },
+  {
+    "revision": "1d76492157b50aa2fc5cbc98fc137ab8",
+    "url": "/reactwebsite2/static/media/img7.1d764921.jpg"
+  },
+  {
+    "revision": "bcba7c0151fd9018727d412a6f74ab8e",
+    "url": "/reactwebsite2/static/media/img8.bcba7c01.jpg"
+  }
+]);
